4a4b70e9497dbcde4943a74708ac8ddd6df0ea76
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2019-03-24 14:29:26 +0100

    latest change

--- opdracht1/form_1.php
+++ opdracht1/form_1.php
@@ -5,13 +5,13 @@
 	$email = $_GET['email'];
 
 	echo "<h2>Response Demo Form</h2><h3>";
-	echo "You submitted the following information<br><ul>";
-	echo "<li>Name: <strong> $firstName</strong></li>";
-	echo "<li>Last Name: <strong> $lastName</strong></li>";
-	echo "<li>Age: <strong> $age</strong></li>";
-	echo "<li>Email: <strong> $email</strong></li>";
+	echo "You submitted the following information<br>";
+	echo "Name: <strong> $firstName</strong> <br>";
+	echo "Last Name: <strong> $lastName</strong><br>";
+	echo "Age: <strong> $age</strong><br>";
+	echo "Email: <strong> $email</strong><br>";
 	echo "form recieved at: <br>";
 	echo "time: ", date("g"), ":", date("i"), ":", date("s");
 	echo "<br>date: ", date("d"), ".", date("m"), ".", date("Y");
-	echo "</li></ul></h3>";
+	echo "</h3>";
 ?>
